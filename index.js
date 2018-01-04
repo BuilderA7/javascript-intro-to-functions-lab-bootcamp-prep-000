d536a66c4560279f706f85cbabb32303d4ed40a4
A I <atbisrael@gmail.com>
2018-01-04 10:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,13 +30,21 @@ function sayHiToGrandma(string){
 
 
 
+<<<<<<< HEAD
   if (string.toLowerCase() === string){
+=======
+  if (message.toLowerCase() === string){
+>>>>>>> 4b9d61eb28b071ba87408862c6c7b9ed7b819c77
     
     const words = 'I can\'t hear you!';
     return words;
   }
   
+<<<<<<< HEAD
  if (string.toUpperCase() === string) {
+=======
+ if (message.toUpperCase() === string) {
+>>>>>>> 4b9d61eb28b071ba87408862c6c7b9ed7b819c77
     
     
     const message1 ='YES INDEED!';
@@ -44,7 +52,11 @@ function sayHiToGrandma(string){
     
   }  
   
+<<<<<<< HEAD
  if (string === "I love you, Grandma."){
+=======
+ if (message === "I love you, Grandma."){
+>>>>>>> 4b9d61eb28b071ba87408862c6c7b9ed7b819c77
   
   const message2 = "I love you, too.";
     return message2 
